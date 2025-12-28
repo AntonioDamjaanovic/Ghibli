7eb128421ff34e158c645924f56f1cc8654077dd
AntonioDamjaanovic <damjanovic1109@gmail.com>
2025-12-28 16:53:37 +0100

    Initial Commit

---
+++ Ghibli/GhibliApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliApp.swift
+//  Ghibli
+//
+//  Created by Antonio Damjanović on 28.12.2025..
+//
+
+import SwiftUI
+
+@main
+struct GhibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhibliTests/GhibliTests.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliTests.swift
+//  GhibliTests
+//
+//  Created by Antonio Damjanović on 28.12.2025..
+//
+
+import Testing
+@testable import Ghibli
+
+struct GhibliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
